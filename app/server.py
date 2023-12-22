0f7cddee00e23ddf6ed40e609ec62cd183b0578f
Romain <romain.pipon@etu.univ-nantes.fr>
2023-12-22 09:10:34 +0100

    fix(testcomm/socket): Create multiple instance of modbusClient for different web client

--- app/server.py
+++ app/server.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from modules.ModbusClient import ModbusClient
 import json
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+rooms = {}
 
 
 @app.route('/')
@@ -17,69 +19,83 @@ def testcomm():
 def webdynconfig():
     return render_template('routes/webdynconfig.html')
 
+
 @app.route('/suivilogs')
 def suivilogs():
     return render_template('routes/suivilogs.html')
+
 
 @app.route('/pontbascule')
 def pontbascule():
     return render_template('routes/pontbascule.html')
 
+
 @app.route('/webdynemul')
 def webdynemul():
     return render_template('routes/webdynemul.html')
+
 
 @app.route('/config')
 def config():
     return render_template('routes/config.html')
 
 
-modbusClient = None
+@socketio.on('connect')
+def handle_connect():
+    global rooms
+    rooms[request.sid] = None
+    print(f"Client {request.sid} connected")
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    global rooms
+    del rooms[request.sid]
+    print(f"Client {request.sid} disconnected")
 
 
 @socketio.on('modbus')
 def handle_modbus(message):
-    global modbusClient
-
-    request = json.loads(message)
-    action = request['action']
+    global rooms
+    payload = json.loads(message)
+    action = payload['action']
 
     if action == 'connect':
-        ip = request['ip']
-        port = request['port']
-        if (isinstance(modbusClient, ModbusClient)):
-            modbusClient.disconnect()
-        modbusClient = ModbusClient(ip=ip, port=port)
+        ip = payload['ip']
+        port = payload['port']
+        if (isinstance(rooms[request.sid], ModbusClient)):
+            rooms[request.sid].disconnect()
+        rooms[request.sid] = ModbusClient(ip=ip, port=port)
 
-        if modbusClient.connect():
+        if rooms[request.sid].connect():
             socketio.emit('modbus', json.dumps({'action': 'connect', 'status': True, 'data': f"Connected to {ip}:{port}"}))
         else:
             socketio.emit('modbus', json.dumps({'action': 'connect', 'status': False, 'data': f"Failed to connect to {ip}:{port}"}))
 
     elif action == 'disconnect':
-        if (isinstance(modbusClient, ModbusClient)):
-            modbusClient.disconnect()
-            modbusClient = None
+        if (isinstance(rooms[request.sid], ModbusClient)):
+            rooms[request.sid].disconnect()
+            rooms[request.sid] = None
             socketio.emit('modbus', json.dumps({'action': 'disconnect', 'status': True, 'data': "Disconnected"}))
         else:
             socketio.emit('modbus', json.dumps({'action': 'disconnect', 'status': False, 'data': "Not connected"}))
 
     elif action == 'read':
-        if (not isinstance(modbusClient, ModbusClient)):
+        if (not isinstance(rooms[request.sid], ModbusClient)):
             socketio.emit('modbus', json.dumps({'action': 'read', 'status': False, 'data': "Not connected"}))
         else:
-            type_register = request['type']
-            address = request['address']
-            count = request['count']
+            type_register = payload['type']
+            address = payload['address']
+            count = payload['count']
 
             if type_register == 'coil':
-                data = modbusClient.read_coils(address, count)
+                data = rooms[request.sid].read_coils(address, count)
             elif type_register == 'discrete':
-                data = modbusClient.read_discrete_inputs(address, count)
+                data = rooms[request.sid].read_discrete_inputs(address, count)
             elif type_register == 'input':
-                data = modbusClient.read_input_registers(address, count)
+                data = rooms[request.sid].read_input_registers(address, count)
             elif type_register == 'holding':
-                data = modbusClient.read_holding_registers(address, count)
+                data = rooms[request.sid].read_holding_registers(address, count)
             else:
                 socketio.emit('modbus', json.dumps({'action': 'read', 'status': False, 'data': "Unknown type"}))
 
