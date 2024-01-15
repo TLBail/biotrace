c1d5698344593c7615ad2962eef4f31a85abb420
Romain <romain.pipon@etu.univ-nantes.fr>
2024-01-15 19:27:44 +0100

    API: get config from db and display on the code editor

--- app/api/webdynconfig.py
+++ app/api/webdynconfig.py
@@ -12,6 +12,14 @@ def get_configs():
     data = db.get_configs(10)
     json_data = []
     for result in data:
-        json_data.append({'id': result[0], 'name': result[1], 'type': result[2], 'content': str(result[3]), 'date': result[4].strftime('%Y-%m-%d'), 'updateAt': result[5].strftime('%Y-%m-%d'), 'deletteAt': result[6].strftime('%Y-%m-%d') if result[6] != None else None})
+        id = result[0]
+        type = result[1]
+        name = result[2]
+        content = str(result[3]).removeprefix("b'").removesuffix("'")
+        date = result[4].strftime('%Y-%m-%d')
+        updateAt = result[5].strftime('%Y-%m-%d')
+        deletteAt = result[6].strftime('%Y-%m-%d') if result[6] is not None else None
+
+        json_data.append({'id': id, 'name': name, 'type': type, 'content': content, 'date': date, 'updateAt': updateAt, 'deletteAt': deletteAt})
 
     return jsonify(json_data)
