d07dbc3d226db7fa68b95844ef83a81751946164
Romain <romain.pipon@etu.univ-nantes.fr>
2024-01-16 06:54:09 +0100

    fix(module/Database.py): Correction de la requete sql, récuperer seulement les configs non supprimé

--- app/modules/Database.py
+++ app/modules/Database.py
@@ -15,7 +15,7 @@ class Database:
         self.cur = self.conn.cursor()
 
     def get_configs(self, n):
-        self.cur.execute("SELECT * FROM file WHERE type='config' ORDER BY id DESC LIMIT " + str(n))
+        self.cur.execute("SELECT * FROM file WHERE type='config' and deleted_at is NULL ORDER BY id DESC LIMIT " + str(n))
         rows = self.cur.fetchall()
 
         return rows
