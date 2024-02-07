22e6fe3f44c5c21be7b880df502d3d77c2242329
tholeb <43311443+tholeb@users.noreply.github.com>
2024-02-07 17:43:15 +0100

    ref : database

--- app/modules/Database.py
+++ app/modules/Database.py
@@ -2,31 +2,37 @@ import mariadb
 
 
 class Database:
-    def __init__(self):
-        self.config = {
-            'host': '127.0.0.1',
-            'port': 3306,
-            'user': 'dev',
-            'password': 'dev',
-            'database': 'Biotrace'
-        }
+	def __init__(self):
+		self.config = {
+			'host': '127.0.0.1',
+			'port': 3306,
+			'user': 'dev',
+			'password': 'dev',
+			'database': 'Biotrace'
+		}
 
-        self.conn = mariadb.connect(**self.config)
-        self.cur = self.conn.cursor()
+		self.conn = mariadb.connect(**self.config)
+		self.cur = self.conn.cursor()
 
-    def get_configs(self, n):
-        self.cur.execute("SELECT * FROM file WHERE type='config' and deleted_at is NULL ORDER BY id DESC LIMIT " + str(n))
-        rows = self.cur.fetchall()
+	def get_hashed_configs(self, n = 10, cols : list[str] = ["*"]):
+		self.cur.execute(f"SELECT {cols[0] if cols == ['*'] else ', '.join(cols)} FROM file WHERE type='config' AND deleted_at IS NULL ORDER BY id DESC LIMIT {str(n)}")
+		rows = self.cur.fetchall()
 
-        return rows
+		return rows
 
-    def add_config(self, name, content):
-        # try to insert
-        try:
-            self.cur.execute("INSERT INTO file (type, name, content, created_at, updated_at) VALUES ('config', ?, ?, now(), now())", (name, content))
-            self.conn.commit()
-        except mariadb.Error as e:
-            print(f"Error: {e}")
+	def get_configs(self, n = 10, cols : list[str] = ["*"]):
+		self.cur.execute(f"SELECT {cols[0] if cols == ['*'] else ', '.join(cols)} FROM file WHERE type='config' and deleted_at is NULL ORDER BY id DESC LIMIT {str(n)}")
+		rows = self.cur.fetchall()
 
-    def __del__(self):
-        self.conn.close()
+		return rows
+
+	def add_config(self, name, content):
+		# try to insert
+		try:
+			self.cur.execute("INSERT INTO file (type, name, content, created_at, updated_at) VALUES ('config', ?, ?, now(), now())", (name, content))
+			self.conn.commit()
+		except mariadb.Error as e:
+			print(f"Error: {e}")
+
+	def __del__(self):
+		self.conn.close()
