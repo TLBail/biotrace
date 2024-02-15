d54941d9a91d92d537104edff6fbea118c101b0e
tholeb <43311443+tholeb@users.noreply.github.com>
2024-02-15 11:05:46 +0100

    DB : update schema to have default values and not null

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,14 @@
 CREATE DATABASE IF NOT EXISTS Biotrace;
-
 USE Biotrace;
-
 -- TODO: Change in prod
 CREATE USER IF NOT EXISTS 'dev'@'localhost' IDENTIFIED BY 'dev';
-
 GRANT ALL PRIVILEGES ON Biotrace.* to 'dev'@'localhost';
-
-
 CREATE TABLE IF NOT EXISTS `file` (
 	`id` int PRIMARY KEY AUTO_INCREMENT,
-	`type` ENUM ('log', 'config'),
-	`name` varchar(255),
-	`content` blob,
-	`created_at` date,
-	`updated_at` date,
-	`deleted_at` date COMMENT 'paranoid table'
+	`type` ENUM ('log', 'config') NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`content` BLOB NOT NULL,
+	`created_at` datetime default now() NOT NULL,
+	`updated_at` datetime default now() NOT NULL,
+	`deleted_at` datetime COMMENT 'paranoid table' default NULL
 );
