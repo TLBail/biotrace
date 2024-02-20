6ff300538786dcee9b7170a5a46b34dd383c278d
tholeb <43311443+tholeb@users.noreply.github.com>
2024-02-20 08:39:57 +0100

    Ref: procedure et trigger Parce que mariadb ne support pas "AFTER INSERT OR UPDATE" Add: extensions

--- db/trigger-configs.sql
+++ db/trigger-configs.sql
@@ -2,33 +2,45 @@ USE Biotrace;
 
 DELIMITER //
 
-CREATE TRIGGER file_insert_update_trigger
-AFTER INSERT OR UPDATE ON file
-FOR EACH ROW
+CREATE OR REPLACE PROCEDURE update_file_trigger_logic()
 BEGIN
     DECLARE X_rows INT;
     SET X_rows = 10; -- Change this to the desired number of rows
 
-    IF NEW.deletedAt IS NULL THEN
+    IF NEW.deleted_at IS NULL THEN
         -- Count the total number of rows where deletedAt is null
-        SET @total_null_rows = (SELECT COUNT(*) FROM file WHERE deletedAt IS NULL);
+        SET @total_null_rows = (SELECT COUNT(*) FROM file WHERE deleted_at IS NULL);
 
         IF @total_null_rows > X_rows THEN
             -- Update the oldest row with deletedAt not null
             UPDATE file
-            SET deletedAt = NOW()
-            WHERE deletedAt IS NOT NULL
+            SET deleted_at = NOW()
+            WHERE deleted_at IS NOT NULL
             ORDER BY createdAt
             LIMIT 1;
         ELSE
             -- Update the youngest row with deletedAt not null and set it to NULL
             UPDATE file
-            SET deletedAt = NULL
-            WHERE deletedAt IS NOT NULL
+            SET deleted_at = NULL
+            WHERE deleted_at IS NOT NULL
             ORDER BY createdAt DESC
             LIMIT 1;
         END IF;
     END IF;
-END //
+END//
+
+CREATE TRIGGER file_insert_trigger
+AFTER INSERT ON file
+FOR EACH ROW
+BEGIN
+    CALL update_file_trigger_logic();
+END//
+
+CREATE TRIGGER file_update_trigger
+AFTER UPDATE ON file
+FOR EACH ROW
+BEGIN
+    CALL update_file_trigger_logic();
+END//
 
 DELIMITER ;
