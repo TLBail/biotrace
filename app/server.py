2ef8921af58ba039b1565ee752555534d56b9e59
tholeb <43311443+tholeb@users.noreply.github.com>
2024-03-01 11:27:01 +0100

    Ref: config.ini

--- app/server.py
+++ app/server.py
@@ -1,13 +1,14 @@
+from api.webdynlog import bp as webdynlog_bp
+from api.webdynconfig import bp as webdynconfig_bp
+from modules.Database import init_engine, init_db, db_session, db_models
 from flask import Flask, Blueprint, render_template
 from flask_socketio import SocketIO
 from sockets.commsocket import commsocket
 import rocher
-import os
+from configparser import ConfigParser
 
-from api.webdynconfig import bp as webdynconfig_bp
-from api.webdynlog import bp as webdynlog_bp
-from modules.Database import init_engine, init_db, db_session
-from models.File import File
+config = ConfigParser()
+config.read('config.ini')
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -21,14 +22,7 @@ blueprint = Blueprint(
 )
 app.register_blueprint(blueprint)
 
-# Register the API blueprint
-user = os.getenv('DB_USER', 'dev')
-password = os.getenv('DB_PASSWORD', 'dev')
-host = os.getenv('DB_HOST', 'localhost')
-port = int(os.getenv('DB_PORT', '3306'))
-database = os.getenv('DB_DATABASE', 'Biotrace')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mariadb+mariadbconnector://{user}:{password}@{host}:{port}/{database}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mariadb+mariadbconnector://{config['database']['user']}:{config['database']['password']}@{config['database']['host']}:{config['database']['port']}/{config['database']['name']}"
 
 init_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 init_db()
@@ -45,7 +39,7 @@ def testcomm():
 
 @app.route('/webdynconfig')
 def webdynconfig():
-	configs = db_session.query(File).filter(File.type == 'config').order_by(File.id.desc()).all()
+	configs = db_session.query(db_models["File"]).filter(db_models["File"].type == 'config').order_by(db_models["File"].id.desc()).all()
 	last_config = configs[0].serialize() if len(configs) > 0 else None
 
 	return render_template('routes/webdynconfig.jinja', configs=[config.serialize() for config in configs], last_config=last_config)
@@ -67,11 +61,11 @@ def webdynemul():
 
 
 @app.route('/config')
-def config():
+def webdynconfug():
 	return render_template('routes/config.jinja')
 
 
 if __name__ == '__main__':
-	socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+	socketio.run(app, debug=True, host='0.0.0.0', port=5000, use_reloader=True, log_output=True)
 	app.run(debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
 	print("Server running on port 5000")
