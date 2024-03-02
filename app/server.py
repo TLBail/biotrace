0b5133a12ee205fbe722aaee91eca7c591231682
tholeb <43311443+tholeb@users.noreply.github.com>
2024-03-02 15:23:24 +0100

    ref: app debug config

--- app/server.py
+++ app/server.py
@@ -66,6 +66,6 @@ def webdynconfug():
 
 
 if __name__ == '__main__':
-	socketio.run(app, debug=True, host='0.0.0.0', port=5000, use_reloader=True, log_output=True)
-	app.run(debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+	socketio.run(app, debug=config['app'].getboolean('debug', False), host='0.0.0.0', port=5000, use_reloader=True, log_output=True)
+	app.run(debug=True, host='0.0.0.0', port=5000)
 	print("Server running on port 5000")
